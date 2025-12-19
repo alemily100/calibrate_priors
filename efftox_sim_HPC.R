ff410e94513d64987fe2111e2f1e3e844c7aec35
Emily Alger <emilyl.alger@gmail.com>
2025-12-19 11:46:34 +0000

    ensemble simulations

--- efftox_sim_HPC.R
+++ efftox_sim_HPC.R
@@ -70,15 +70,18 @@ cohortsize<- 4
 ncohort<- 48
 rar.prop<- 0.25
 
-true_tox<- rbind.data.frame(c(0.05, 0.1, 0.2, 0.28, 0.5, 0.5), c(0.05, 0.1, 0.2, 0.28, 0.4, 0.55), 
-                            c(0.05, 0.1, 0.15, 0.2, 0.35, 0.4), c(0.05, 0.05, 0.05, 0.05, 0.05, 0.05), rep(0.05, times=6), c(0.33, 0.45, 0.55, 0.6, 0.7, 0.75))
+true_tox<- rbind.data.frame(c(0.33, 0.45, 0.55, 0.60, 0.70, 0.75), c(0.25, 0.33, 0.45, 0.55, 0.60, 0.70), c(0.15, 0.25, 0.33, 0.45, 0.55, 0.60),
+                            c(0.10, 0.15, 0.25, 0.33, 0.45, 0.55), c(0.05, 0.10, 0.15, 0.25, 0.33, 0.45), c(0.01, 0.05, 0.10, 0.15, 0.25, 0.33),
+                            c(0.01, 0.05, 0.10, 0.15, 0.18, 0.25))
 dimnames(true_tox)[[2]]<- c( "1","2", "3", "4", "5", "6")
-dimnames(true_tox)[[1]]<- c("Sc 1", "Sc 2", "Sc 3", "Sc 4", "Sc5", "Sc6")                       
+dimnames(true_tox)[[1]]<- c("CLIN: Sc 1", "CLIN: Sc 2", "CLIN: Sc 3", "CLIN: Sc 4", "CLIN: Sc5", "CLIN: Sc6", "CLIN: Sc7")                       
 
-true_eff<- rbind.data.frame(c(0.05, 0.13, 0.25, 0.38, 0.5, 0.63), c(0.05, 0.23, 0.47, 0.7, 0.7, 0.7), c(0.7, 0.7, 0.7, 0.7, 0.7, 0.7),
-                            c(0.7, 0.43, 0.38, 0.25, 0.23, 0.05), c(0.38, 0.50, 0.63, 0.5, 0.38, 0.25))
+true_eff<- rbind.data.frame(rep(0.63, times=6), c(0.63, 0.50, 0.38, 0.25, 0.13, 0.05), c(0.5, rep(0.63, times=5)), c(0.5, 0.63, 0.5, 0.38, 0.25, 0.13), 
+                            c(0.38, 0.5, rep(0.63, times=4)), c(0.38, 0.50, 0.63, 0.50, 0.38, 0.25), c(0.25, 0.38, 0.5, rep(0.63, times=3)),
+                            c(0.25, 0.38, 0.5, 0.63, 0.5, 0.38), c(0.13, 0.25, 0.38, 0.5, 0.63, 0.63), c(0.13, 0.25, 0.38, 0.50, 0.63, 0.50),
+                            c(0.05, 0.13, 0.25, 0.38, 0.50, 0.63))
 dimnames(true_eff)[[2]]<- c( "1","2", "3", "4", "5", "6")
-dimnames(true_eff)[[1]]<- c("Sc 1", "Sc 2", "Sc 3", "Sc4", "Sc5")   
+dimnames(true_eff)[[1]]<- c("EFF: Sc 1", "EFF: Sc 2", "EFF: Sc 3", "EFF: Sc4", "EFF: Sc5", "EFF: Sc6", "EFF: Sc7", "EFF: Sc8", "EFF: Sc9", "EFF: Sc10", "EFF: Sc11")   
 
 sim_scenario_k<- matrix(nrow=2, ncol=16)
 sim_scenario_k[1,]<- c(rep(1:4, each=3), rep(5:6, each=2))
@@ -87,9 +90,8 @@ sim_scenario_k[2,]<- c(rep(1:3, times=4), rep(4:5, times=2))
 all_sim<- NULL
 set.seed(1001)
 ntrial=5000
-for(k in 1:16){
-    i<- sim_scenario_k[1,k]
-    j<- sim_scenario_k[2,k]
+for (i in 1:7){
+  for(j in 1:11){
     clin_true<- as.numeric(true_tox[i,])
     eff_true<- as.numeric(true_eff[j,])
     ndose=length(clin_true)
@@ -119,6 +121,7 @@ for(k in 1:16){
     write.csv(n, paste0("/home/ealger/revision_calibrate_priors/results/efftox_results/joint/",el,"HN0.1.sim", i,".",j, ".pat.allocation", ncohort, ".", cohortsize, ".csv"))
     write.csv(yc, paste0("/home/ealger/revision_calibrate_priors/results/efftox_results/joint/",el,"HN0.1.sim", i,".",j, ".", ncohort, ".", cohortsize, ".cdlt.csv"))
     write.csv(ye, paste0("/home/ealger/revision_calibrate_priors/results/efftox_results/joint/",el,"HN0.1.sim",i,".",j, ".", ncohort, ".", cohortsize, ".eff.csv"))
+  }
 }
 write.csv(all_sim, paste0("/home/ealger/revision_calibrate_priors/results/efftox_results/joint/",el,"HN0.1.sim.", ncohort,".", cohortsize,".csv"))
 
@@ -131,9 +134,8 @@ se<- 1.34
 all_sim<- NULL
 set.seed(1001)
 ntrial=5000
-for(k in 1:16){
-  i<- sim_scenario_k[1,k]
-  j<- sim_scenario_k[2,k]
+for (i in 1:7){
+  for(j in 1:11){
   clin_true<- as.numeric(true_tox[i,])
   eff_true<- as.numeric(true_eff[j,])
   ndose=length(clin_true)
@@ -163,6 +165,7 @@ for(k in 1:16){
   write.csv(n, paste0("/home/ealger/revision_calibrate_priors/results/efftox_results/marginal/N1.34/sim", i,".",j, ".pat.allocation", ncohort, ".", cohortsize, ".csv"))
   write.csv(yc, paste0("/home/ealger/revision_calibrate_priors/results/efftox_results/marginal/N1.34/sim", i, ".", j, ".", ncohort, ".", cohortsize, ".cdlt.csv"))
   write.csv(ye, paste0("/home/ealger/revision_calibrate_priors/results/efftox_results/marginal/N1.34/sim", i, ".", j, ".", ncohort, ".", cohortsize, ".eff.csv"))
+  }
 }
 write.csv(all_sim, paste0("/home/ealger/revision_calibrate_priors/results/efftox_results/marginal/N1.34/sim.", ncohort,".", cohortsize,".csv"))
 
@@ -175,7 +178,8 @@ se<- 0.1
 all_sim<- NULL
 set.seed(1001)
 ntrial=5000
-for(k in 1:16){
+for (i in 1:7){
+  for(j in 1:11){
   i<- sim_scenario_k[1,k]
   j<- sim_scenario_k[2,k]
   clin_true<- as.numeric(true_tox[i,])
@@ -207,6 +211,7 @@ for(k in 1:16){
   write.csv(n, paste0("/home/ealger/revision_calibrate_priors/results/efftox_results/marginal/HN0.1/sim", i,".",j, ".pat.allocation", ncohort, ".", cohortsize, ".csv"))
   write.csv(yc, paste0("/home/ealger/revision_calibrate_priors/results/efftox_results/marginal/HN0.1/sim", i, ".", j, ".", ncohort, ".", cohortsize, ".cdlt.csv"))
   write.csv(ye, paste0("/home/ealger/revision_calibrate_priors/results/efftox_results/marginal/HN0.1/sim", i, ".", j, ".", ncohort, ".", cohortsize, ".eff.csv"))
+  }
 }
 write.csv(all_sim, paste0("/home/ealger/revision_calibrate_priors/results/efftox_results/marginal/HN0.1/sim.", ncohort,".", cohortsize,".csv"))
 
