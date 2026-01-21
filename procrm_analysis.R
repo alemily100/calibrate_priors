f9d3890c57973912b3b1ffafaeb907c4c21f08d4
Emily Alger <emilyl.alger@gmail.com>
2026-01-21 11:24:36 +0000

    look at proportion over/under/correctly dosed

--- procrm_analysis.R
+++ procrm_analysis.R
@@ -1,7 +1,7 @@
 setwd("C:/Users/ealger/OneDrive - The Institute of Cancer Research/M/PhD/Trial Designs/calibrate_priors/Statistics in Medicine/revision/code/calibrate_priors")
-joint<-read.csv("C:/Users/ealger/OneDrive - The Institute of Cancer Research/M/PhD/Trial Designs/calibrate_priors/Statistics in Medicine/revision/code/calibrate_priors/results/procrm_results/joint/clay3.39.3.csv")[,-1]
-marginal_gamma<-read.csv("C:/Users/ealger/OneDrive - The Institute of Cancer Research/M/PhD/Trial Designs/calibrate_priors/Statistics in Medicine/revision/code/calibrate_priors/results/procrm_results/marginal_gamma/clay3.39.3.csv")[,-1]
-marginal_lognormal<-read.csv("C:/Users/ealger/OneDrive - The Institute of Cancer Research/M/PhD/Trial Designs/calibrate_priors/Statistics in Medicine/revision/code/calibrate_priors/results/procrm_results/marginal_lognormal/clay3.39.3.csv")[,-1]
+joint<-read.csv("C:/Users/ealger/OneDrive - The Institute of Cancer Research/M/PhD/Trial Designs/calibrate_priors/Statistics in Medicine/revision/code/calibrate_priors/results/procrm_results/joint/clay0.9.39.3.csv")[,-1]
+marginal_gamma<-read.csv("C:/Users/ealger/OneDrive - The Institute of Cancer Research/M/PhD/Trial Designs/calibrate_priors/Statistics in Medicine/revision/code/calibrate_priors/results/procrm_results/marginal_gamma/clay0.9.39.3.csv")[,-1]
+marginal_lognormal<-read.csv("C:/Users/ealger/OneDrive - The Institute of Cancer Research/M/PhD/Trial Designs/calibrate_priors/Statistics in Medicine/revision/code/calibrate_priors/results/procrm_results/marginal_lognormal/clay0.9.39.3.csv")[,-1]
 
 cdlt.rec<-sapply(((0:35)*3)+1, function (k) which.min(abs(joint[k,]-0.25)))
 pdlt.rec<-sapply(((0:35)*3)+2, function (k) which.min(abs(joint[k,]-0.35)))
@@ -34,3 +34,49 @@ random.joint<-joint[row_index,]
 random.marginal<-marginal_gamma[row_index,]
 write.csv(random.joint, "tables/procrm.random.joint.csv")
 write.csv(random.marginal, "tables/procrm.random.marginal.csv")
+
+key<- data.frame(nrow=36, ncol=2)
+ind<-1
+for (i in 1:6){
+  for(j in 1:6){
+    key[ind,]<- c(ind,paste0("clay0.9.sim",i,".",j,".39.3.csv"))
+  ind<-ind+1}
+}
+key[sort(scenarios),2]
+
+
+joint_folder_path<-"C:/Users/ealger/OneDrive - The Institute of Cancer Research/M/PhD/Trial Designs/calibrate_priors/Statistics in Medicine/revision/code/calibrate_priors/results/procrm_results/joint/"
+marginal_folder_path<-"C:/Users/ealger/OneDrive - The Institute of Cancer Research/M/PhD/Trial Designs/calibrate_priors/Statistics in Medicine/revision/code/calibrate_priors/results/procrm_results/marginal_gamma/"
+
+for (i in 1:12){
+  eval(parse(text=paste0('joint',i,'<-read.csv(paste0(joint_folder_path, key[sort(scenarios),2][',i,']))[,-1]')))
+  eval(parse(text=paste0('marginal',i,'<-read.csv(paste0(marginal_folder_path, key[sort(scenarios),2][',i,']))[,-1]')))
+}
+
+prop.pat.joint<-NULL
+prop.pat.marg<-NULL
+correctdose<-optimal.rec[sort(scenarios)]
+underdose<- sapply(sort(scenarios), function (k) (min(1,optimal.rec[k]-1):(optimal.rec[k]-1)))
+overdose<- sapply(sort(scenarios), function (k) ((optimal.rec[k]+1):max(5,optimal.rec[k]+1)))
+
+for (i in 1:12){
+  eval(parse(text=paste0("under<-sum(colMeans(joint",i,"/rowSums(joint",i,"))[underdose[",i,"][[1]]])")))
+  eval(parse(text=paste0("over<-sum(colMeans(joint",i,"/rowSums(joint",i,"))[overdose[",i,"][[1]]])")))
+  eval(parse(text=paste0("best<-sum(colMeans(joint",i,"/rowSums(joint",i,"))[correctdose[",i,"]])")))
+  row<-cbind.data.frame(best, under, over)
+  prop.pat.joint<-rbind.data.frame(prop.pat.joint, row)
+}
+
+for (i in 1:12){
+  eval(parse(text=paste0("under<-sum(colMeans(marginal",i,"/rowSums(marginal",i,"))[underdose[",i,"][[1]]])")))
+  eval(parse(text=paste0("over<-sum(colMeans(marginal",i,"/rowSums(marginal",i,"))[overdose[",i,"][[1]]])")))
+  eval(parse(text=paste0("best<-sum(colMeans(marginal",i,"/rowSums(marginal",i,"))[correctdose[",i,"]])")))
+  row<-cbind.data.frame(best, under, over)
+  prop.pat.marg<-rbind.data.frame(prop.pat.marg, row)
+}
+
+write.csv(prop.pat.joint, "tables/procrm.prop.pat.joint.csv")
+write.csv(prop.pat.marg, "tables/procrm.prop.pat.marginal.csv")
+
+
+                   
